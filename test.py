b6d6afde81fa9cceec2a8cb86c12340e9313303e
Piyush Ghate <piyushghate17@gmail.com>
2018-11-10 21:49:16 +0530

    working with stargazers_count

--- test.py
+++ test.py
@@ -1,15 +1,27 @@
-import requests
-import json
+# import requests
+# import json
 
-link = "https://api.github.com/users/piyushghate/repos"
+# link = "https://api.github.com/users/piyushghate/repos"
 # f = requests.get(link)
 # data = json.loads(f.read())
 # print(data)
 
-r = requests.get(link)
-print(r.json())
+# r = requests.get(link)
+# # print(r.json())
 
 # with open('data.json', 'w') as testfile:
-#     json.dump(r, testfile)
+#     json.dumps(r)
 
 
+import urllib.request, json 
+with urllib.request.urlopen("https://api.github.com/users/piyushghate/repos") as url:
+    data = json.loads(url.read().decode())
+    # print(data)
+
+    # with open('data.json', 'w') as testfile:
+    #     json.dump(data, testfile)
+
+
+for each in data:
+    if (each['stargazers_count'] == 1):
+        print(each['id'])
