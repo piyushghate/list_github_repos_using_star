6f6fefeee93cf628346e461479cb0de04b27a306
Piyush Ghate <piyushghate17@gmail.com>
2018-11-13 09:22:50 +0530

    made it simple

--- testinput.py
+++ testinput.py
@@ -1,35 +1,6 @@
 import json
 
+inputfromuser = input("Please mention the org name: ")      #org name from user used directly in the url below
 
-inputfromuser = input("Please mention the org name: ")
-
-inputobj = {
-    'org' : inputfromuser, 
-}
-
-inputjson = json.dumps(inputobj)
-print("Input given as: ",inputjson)
-
-# print(type(inputjson))
-print("InputJSON:", inputjson)
-
-
-
-
-# jsonstring = {"org" : "mozilla"}
-# print(type(jsonstring))
-
-
-
-# print("jsonstring",json.dumps(jsonstring))
-
-inputlist1 = []
-
-inputlist1.append(inputobj)
-print(inputlist1)
-
-orgname = inputlist1[0]["org"]
-# print(orgname)
-
-url1 = "https://api.github.com/orgs/" +orgname+"/repos"
+url1 = "https://api.github.com/orgs/" +inputfromuser+"/repos"
 print(url1)
