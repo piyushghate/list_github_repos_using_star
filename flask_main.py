5fb96860e578742c8ff4cbd0545d7297bd5dba99
piyush ghate <piyushghate17@gmail.com>
2018-11-13 17:21:55 +0530

    few comments added to flask_main file

--- flask_main.py
+++ flask_main.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 @app.route('/repos', methods = ['POST'])    # defines the method and path to the api
 def index():
     if(request.method == 'POST'):
-        return_from_mainapp = run_api(request.get_json())
-        return jsonify({'results': return_from_mainapp}), 201
+        return_from_mainapp = run_api(request.get_json())   # function call with the JSON data from client POST request
+        return jsonify({'results': return_from_mainapp}), 201   # converts the data from the function into JSON format and returns to the client with 201 response code.
 
 if __name__ == '__main__':
     app.run(debug=True)
